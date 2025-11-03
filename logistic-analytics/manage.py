417bbeb484d2124ad71839ba4d83c603f3dc5c0b
autoryuzo <trixsociety@mail.ru>
2025-11-03 20:27:41 +0300

    new simplified way to draw graph

--- logistic-analytics/manage.py
+++ logistic-analytics/manage.py
@@ -4,11 +4,11 @@ import pandas as pd
 import numpy as np
 from shapely.geometry import Point
 from sklearn.cluster import DBSCAN
+import networkx as nx
 
 # -------------------------
 # Настройки
 # -------------------------
-# bbox Казани (west, south, east, north)
 bbox_kazan = (48.8, 55.6, 49.3, 55.9)
 
 tags_logistics = {
@@ -26,68 +26,99 @@ if centers_gdf.empty:
     print("Не найдено логистических центров.")
     exit()
 
-# Получаем координаты центров (центроид для полигона)
+# -------------------------
+# 2) Получаем координаты и информацию по каждому объекту
+# -------------------------
 coords = []
-for geom in centers_gdf.geometry:
+for idx, row in centers_gdf.iterrows():
+    geom = row.geometry
     if geom.geom_type in ['Polygon', 'MultiPolygon']:
-        coords.append([geom.centroid.y, geom.centroid.x])
+        y, x = geom.centroid.y, geom.centroid.x
     else:
-        coords.append([geom.y, geom.x])
+        y, x = geom.y, geom.x
+    coords.append({
+        'lat': y,
+        'lon': x,
+        'tags': row.to_dict()
+    })
 
-coords = pd.DataFrame(coords, columns=['lat', 'lon'])
+coords_df = pd.DataFrame(coords)
 
 # -------------------------
-# 2) Кластеризация точек (DBSCAN)
+# 3) Кластеризация точек (DBSCAN)
 # -------------------------
 kms_per_radian = 6371.0088
 epsilon = 0.15 / kms_per_radian  # 150 метров
 
-coords_rad = np.radians(coords[['lat','lon']])
+coords_rad = np.radians(coords_df[['lat','lon']])
 db = DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine').fit(coords_rad)
-coords['cluster'] = db.labels_
+coords_df['cluster'] = db.labels_
 
-# агрегируем центроиды кластеров
-clustered_centers = coords.groupby('cluster').mean().reset_index()
+# -------------------------
+# 4) Агрегируем центроиды и информацию по кластерам
+# -------------------------
+clustered_centers = coords_df.groupby('cluster').agg({
+    'lat': 'mean',
+    'lon': 'mean',
+    'tags': lambda x: list(x)
+}).reset_index()
+
 clustered_centers['name'] = ['Логистический центр ' + str(i+1) for i in clustered_centers.index]
 print(f"После кластеризации центров: {len(clustered_centers)}")
 
 # -------------------------
-# 3) Загружаем дорожную сеть
+# 5) Загружаем дорожную сеть (drive)
 # -------------------------
-print("Загружаем дорожную сеть (drive)...")
+print("Загружаем дорожную сеть...")
 G_drive = ox.graph_from_bbox(
     bbox=bbox_kazan,
     network_type='drive'
 )
-# длины рёбер ('length') уже есть в графе
 
 # -------------------------
-# 4) Находим ближайшие узлы к центрам
+# 6) Строим граф расстояний между кластерами
 # -------------------------
-clustered_centers['node'] = clustered_centers.apply(
-    lambda row: ox.distance.nearest_nodes(G_drive, X=row['lon'], Y=row['lat']), axis=1
-)
+edges = []
+nodes = clustered_centers.index.tolist()
 
-# -------------------------
-# 5) Строим кратчайшие маршруты между центрами
-# -------------------------
-routes = []
-for i, row1 in clustered_centers.iterrows():
-    for j, row2 in clustered_centers.iterrows():
+for i in nodes:
+    for j in nodes:
         if i < j:
-            route = ox.shortest_path(G_drive, row1['node'], row2['node'], weight='length')
-            routes.append((i, j, route))
+            try:
+                node_i = ox.distance.nearest_nodes(G_drive, X=clustered_centers.loc[i,'lon'], Y=clustered_centers.loc[i,'lat'])
+                node_j = ox.distance.nearest_nodes(G_drive, X=clustered_centers.loc[j,'lon'], Y=clustered_centers.loc[j,'lat'])
+                length = nx.shortest_path_length(G_drive, node_i, node_j, weight='length')
+                edges.append((i, j, {'weight': length}))
+            except:
+                # если путь не найден, пропускаем
+                continue
+
+# создаём граф NetworkX
+G_clusters = nx.Graph()
+G_clusters.add_nodes_from(nodes)
+G_clusters.add_edges_from(edges)
 
 # -------------------------
-# 6) Визуализация на Folium
+# 7) Строим минимальный остов (MST) для упрощённого графа
+# -------------------------
+mst = nx.minimum_spanning_tree(G_clusters)
+
+# -------------------------
+# 8) Визуализация на Folium
 # -------------------------
 m = folium.Map(
     location=[(bbox_kazan[1]+bbox_kazan[3])/2, (bbox_kazan[0]+bbox_kazan[2])/2],
     zoom_start=11
 )
 
-# добавляем центры
+# добавляем вершины
 for idx, row in clustered_centers.iterrows():
+    popup_html = f"<b>{row['name']}</b><br>"
+    for bld in row['tags']:
+        building_type = bld.get('building', 'неизвестно')
+        name = bld.get('name', '')
+        popup_html += f"{building_type} {name}<br>"
+
     folium.CircleMarker(
         location=[row['lat'], row['lon']],
         radius=6,
@@ -95,13 +126,19 @@ for idx, row in clustered_centers.iterrows():
         fill=True,
         fill_color='red',
         fill_opacity=0.7,
-        popup=row['name']
+        popup=popup_html
     ).add_to(m)
 
-# добавляем маршруты
-for i, j, route in routes:
-    coords_route = [(G_drive.nodes[n]['y'], G_drive.nodes[n]['x']) for n in route]
-    folium.PolyLine(coords_route, color='blue', weight=2, opacity=0.5).add_to(m)
+# добавляем рёбра MST
+for u, v, data in mst.edges(data=True):
+    row_u = clustered_centers.loc[u]
+    row_v = clustered_centers.loc[v]
+    folium.PolyLine(
+        locations=[[row_u['lat'], row_u['lon']], [row_v['lat'], row_v['lon']]],
+        color='blue',
+        weight=2,
+        opacity=0.6
+    ).add_to(m)
 
-m.save("kazan_logistics_graph.html")
-print("Карта с графом сохранена: kazan_logistics_graph.html")
+m.save("kazan_logistics_graph_mst.html")
+print("Карта с MST маршрутов сохранена: kazan_logistics_graph_mst.html")
